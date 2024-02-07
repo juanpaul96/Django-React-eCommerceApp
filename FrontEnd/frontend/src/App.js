fcf16b2d599bc3acc30020e61cf758449e68f13a
JLinaresABA <JLinares@nationalppo.com>
2024-02-06 19:07:17 -0500

    Adding frontend

---
+++ FrontEnd/frontend/src/App.js
@@ -0,0 +1,25 @@
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductSceen from "./screens/ProductSceen";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductSceen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ FrontEnd/frontend/src/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Navbar, Nav, Container, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container fluid>
+          <LinkContainer to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="mr-auto"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Login
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
